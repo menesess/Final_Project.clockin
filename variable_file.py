4fc4947c589ddd01d0d9007c9c6914de5b30d6ec
menesess <sandra_menesess@berea.edu>
2017-12-08 21:08:04 -0500

    All done!...for now...

---
+++ variable_file.py
@@ -0,0 +1,16 @@
+#######################################################################################################
+# Author: Nina Meneses
+# Username: menesess
+#
+# Assignment: Final Project: Email Reminder
+#
+# Purpose: To send Guillermo automated email reminders according to his work schedule
+#
+########################################################################################################
+# Acknowledgements: Python Software Foundation, Stack Overflow, Python Library Reference and Guillermo
+########################################################################################################
+
+TOADDR = "guillermoramos330179@gmail.com"
+FROMADDR = "justcallmemarvin@gmail.com"
+BODY = "Hi, Pollito! Please click on the link below to clock in/out. /n https://timemachine1-vm.berea.edu/UltraTime/UltraPunch/login.aspx?ReturnUrl=%2fultratime%2fultrapunch%2findex.aspx"
+PASSWORD = "butts"
